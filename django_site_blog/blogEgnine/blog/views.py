e4675efa7ff8a95780f187534589a6e94f1e1ad9
Sergey-user <50555348+Sergey-user@users.noreply.github.com>
2019-05-16 00:02:02 +0300

    Изменение интерфейса карты поста и создания страниц тега

--- django_site_blog/blogEgnine/blog/views.py
+++ django_site_blog/blogEgnine/blog/views.py
@@ -12,9 +12,9 @@ def post_detail(request, slug):
     return render(request, 'blog/post_detail.html', context={'post':post})
 
 
-def tags_list(request):
+def tags_list(request): 
     tags = Tag.objects.all()
-    return render(request, 'blog/posts_list.html', context={'tags':tags})
+    return render(request, 'blog/tags_list.html', context={'tags':tags})
 
 def tag_detail(request, slug):
     tag = Tag.objects.get(slug__iexact=slug)
