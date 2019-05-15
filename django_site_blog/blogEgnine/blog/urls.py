b545cc91e4019257e0628c7ceaf90a695cbd90ab
Sergey-user <50555348+Sergey-user@users.noreply.github.com>
2019-05-15 19:17:51 +0300

    Добавление нового функционала(1)
    Добавление возможности чтения поста

--- django_site_blog/blogEgnine/blog/urls.py
+++ django_site_blog/blogEgnine/blog/urls.py
@@ -3,6 +3,6 @@ from blog.views import *
 
 
 urlpatterns = [
-    path('posts', posts_list, name='posts_list_url')
-
+    path('posts', posts_list, name='posts_list_url'),
+    path('post/<str:slug>/', post_detail, name='post_detail_url')
 ]
--- django_site_blog/blogEgnine/blog/views.py
+++ django_site_blog/blogEgnine/blog/views.py
@@ -5,3 +5,7 @@ from blog.models import Post
 def posts_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/posts_list.html', context={'posts':posts})
+
+def post_detail(request, slug):
+    post = Post.objects.get(slug__iexact=slug)
+    return render(request, 'blog/post_detail.html', context={'post':post})
