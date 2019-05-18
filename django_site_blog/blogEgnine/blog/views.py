0766a10bdec52de8b423754f00b4890dce2fad07
Sergey-user <50555348+Sergey-user@users.noreply.github.com>
2019-05-18 13:04:56 +0300

    Добавление нового функционала(4)
    Добавление возможности изменения постов и тегов

--- django_site_blog/blogEgnine/blog/views.py
+++ django_site_blog/blogEgnine/blog/views.py
@@ -11,7 +11,7 @@ def posts_list(request):
 
 def post_detail(request, slug):
     post = Post.objects.get(slug__iexact=slug)
-    return render(request, 'blog/post_detail.html', context={'post':post})
+    return render(request, 'blog/post_detail.html', context={'post':post, 'object_for_admin':post, 'admin_detail':True})
 
 
 def tags_list(request):
@@ -20,7 +20,7 @@ def tags_list(request):
 
 def tag_detail(request, slug):
     tag = Tag.objects.get(slug__iexact=slug)
-    return render(request, 'blog/tag_detail.html', context={'tag':tag})
+    return render(request, 'blog/tag_detail.html', context={'tag':tag, 'object_for_admin':tag, 'admin_detail':True})
 
 
 class TagCreate(View):
@@ -36,6 +36,21 @@ class TagCreate(View):
         return render(request, 'blog/tag_create_form.html', context={'form':bound_form})
 
 
+class TagUpdate(View):
+    def get(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        bound_form = TagForm(instance=tag)
+        return render(request, 'blog/tag_update_form.html', context={'form':bound_form, 'tag':tag})
+
+    def post(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        bound_form = TagForm(request.POST, instance=tag)
+        if bound_form.is_valid():
+            update_tag = bound_form.save()
+            return redirect(update_tag)
+        return render(request, 'blog/tag_update_form.html', context={'form':bound_form, 'tag':tag})
+
+
 class PostCreate(View):
     def get(self, request):
         form = PostForm()
@@ -45,5 +60,31 @@ class PostCreate(View):
         bound_form = PostForm(request.POST)
         if bound_form.is_valid():
             new_post = bound_form.save()
-            return redirect(new_tag)
-        return render(request, 'blog/post_create_form.html', context={'form':form})
+            return redirect(new_post)
+        return render(request, 'blog/post_create_form.html', context={'form':bound_form})
+
+
+class PostUpdate(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug__iexact=slug)
+        bound_form = PostForm(instance=post)
+        return render(request, 'blog/post_update_form.html', context={'form':bound_form, 'post':post})
+
+    def post(self, request, slug):
+        post = Post.objects.get(slug__iexact=slug)
+        bound_form = PostForm(request.POST, instance=post)
+        if bound_form.is_valid():
+            update_post = bound_form.save()
+            return redirect(update_post)
+        return render(request, 'blog/post_update_form.html')
+
+
+class PostDelete(View):
+    def get(self, request, slug):
+        post = Post.objects.get(slug__iexact=slug)
+        return render(request, 'blog/post_delete_form.html', context={'post':post})
+
+    def post(self, request, slug):
+        post = Post.objects.get(slug__iexact=slug)
+        post.delete()
+        return redirect(reverse('posts_list_url'))
